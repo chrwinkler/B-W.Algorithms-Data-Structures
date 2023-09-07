86e0c87f55bfcbffd61def9e11f69c84d9e9d39b
hipdoghotdog <114659437+hipdoghotdog@users.noreply.github.com>
2023-09-07 10:40:29 +0200

    Start

---
+++ Week 1-2/Assignement/Exxercise_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+// int M = rand() % 10;
+int N = rand() % 10;
+int rNum;
+std::vector<int> List;
+for (int i = 0; i < N; i++)
+{
+    int M = rand() % 10;
+    rNum = rand() % 10;
+}
