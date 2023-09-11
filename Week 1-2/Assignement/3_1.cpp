f98a1db804ec75ee499fe5deb37f98a08b458bb9
hipdoghotdog <114659437+hipdoghotdog@users.noreply.github.com>
2023-09-11 15:28:57 +0200

    3 code done

--- Week 1-2/Assignement/3_1.cpp
+++ Week 1-2/Assignement/3_1.cpp
@@ -5,7 +5,7 @@
 int myMethod(int n)
 {
     int sum = 0;
-    for (int i = 1; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
         ++sum;
     }
---
+++ Week 1-2/Assignement/3_2.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+int myMethod(int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            ++sum;
+        }
+    }
+}
+
+int main(void)
+{
+    int N, msec = 0;
+    printf("Input N:");
+    scanf_s("%d", &N);
+    clock_t before = clock();
+    myMethod(N);
+    clock_t duration = clock() - before;
+    msec = duration * 1000 / CLOCKS_PER_SEC;
+    printf("duration in msec: %d", msec);
+    return 0;
+}
