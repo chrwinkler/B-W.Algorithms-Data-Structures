4dbc4b828768cd0e0764365cb199d4ee31d2bee9
hipdoghotdog <114659437+hipdoghotdog@users.noreply.github.com>
2023-09-11 13:54:17 +0200

    Working

--- Week 1-2/Assignement/Exxercise_1.cpp
+++ Week 1-2/Assignement/Exxercise_1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
@@ -8,20 +11,41 @@ int main(void)
 {
 
     // int M = rand() % 10;
-    int N = rand() % 10;
+    int N = rand() % 100;
+    int M = rand() % 100;
     int rNum;
-    std::vector<int> List;
-    std::vector<int>::iterator itr;
+    std::vector<int> MList;
+    std::vector<int> NList;
+    std::vector<int>::iterator Nitr;
+    std::vector<int>::iterator Mitr;
+    int counter = 0;
+    srand(time(NULL));
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < M; i++)
     {
-        int M = rand() % 10;
-        List.push_back(M);
-    }
-
-    for (itr = List.begin(); itr != List.end(); itr++)
-    {
-        cout << *itr << " ";
+        int rNum = rand() % 10;
+        cout << rNum << " ";
+        MList.push_back(rNum);
     }
     cout << endl;
+    for (int i = 0; i < N; i++)
+    {
+        int rNum = rand() % 10;
+        cout << rNum << " ";
+        NList.push_back(rNum);
+    }
+    cout << endl;
+
+    for (Nitr = NList.begin(); Nitr != NList.end(); Nitr++)
+    {
+        for (Mitr = MList.begin(); Mitr != MList.end(); Mitr++)
+        {
+            if (*Nitr == *Mitr)
+            {
+                counter++;
+            }
+        }
+    }
+
+    cout << counter << endl;
 }
