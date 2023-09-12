6787a68b8fa3f824cd7e64705dfeb66d863a3302
chrwinkler <101516854+chrwinkler@users.noreply.github.com>
2023-09-12 11:12:56 +0200

    fff

--- Week 1-2/Assignement/MaxHeap/MaxHeapIMPL.cpp
+++ Week 1-2/Assignement/MaxHeap/MaxHeapIMPL.cpp
@@ -8,23 +8,82 @@ MaxHeapImpl::MaxHeapImpl() {
 }
 
 bool MaxHeapImpl::isEmpty() const {
-    // Implementation for isEmpty
+    return heap.empty();
 }
 
 int MaxHeapImpl::size() {
-    // Implementation for size
+    return heap.size();
 }
 
 void MaxHeapImpl::insert(const int x) {
-    // Implementation for insert
+      // Add the new element to the end of the vector
+    heap.push_back(x);
+
+    // Maintain the max heap property
+    int index = heap.size() - 1;
+    while (index > 0) {
+        int parentIndex = (index - 1) / 2;
+        if (heap[index] > heap[parentIndex]) {
+            std::swap(heap[index], heap[parentIndex]);
+            index = parentIndex;
+        } else {
+            break; // Max heap property is satisfied
+        }
+    }
 }
 
 const int MaxHeapImpl::findMax() const {
-    // Implementation for findMax
+    if (isEmpty())
+        {
+            throw std::runtime_error("Heap is empty");
+        }
+        return heap[0];
 }
 
 int MaxHeapImpl::deleteMax() {
-    // Implementation for deleteMax
+       // If We have an empty heap then we can't find the MaxHeap
+        if (isEmpty())
+        {
+            throw std::runtime_error("Heap is empty");
+        }
+
+        // Swaping the root that is the maximum element with the last element
+        int maxElement = heap[0];
+        heap[0] = heap.back();
+        heap.pop_back();
+
+        // sift down to maintain the max heap property
+        int index = 0;
+        int size = heap.size();
+        while (true)
+        {
+            int leftChild = 2 * index + 1;
+            int rightChild = 2 * index + 2;
+            int largest = index;
+
+            if (leftChild < size && heap[leftChild] > heap[largest])
+            {
+                largest = leftChild;
+            }
+            if (rightChild < size && heap[rightChild] > heap[largest])
+            {
+                largest = rightChild;
+            }
+             
+            if (largest != index)
+            {
+                // Swap the current element with the larger child
+                std::swap(heap[index], heap[largest]);
+                // Update the index to the new position of the element
+                index = largest;
+            }
+            else
+            {
+                break;
+            }
+        }
+        // The MaxHeap has been found 
+        return maxElement;
 }
 
 // Iterator class definition
